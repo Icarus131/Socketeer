2226cc834497fa79c0279f428a2d7ab9f7cd385e
Icarus <couchpotato1311@gmail.com>
2023-01-15 15:06:10 +0530

    Adding banner and splash info function

--- socketeer.py
+++ socketeer.py
@@ -1,24 +1,43 @@
+#!/usr/bin/env python3
+#Author: Icarus (www.icarus.tk)
+#This file is a part of Socketeer
+
+
 import os
-import pytermgui as ptg
+import subprocess as sp
 import time
 import curses
+import sys,traceback
+from curses import wrapper
 
 #Start banner
-
-os.system("echo 
-   _____            __        __                
-  / ___/____  _____/ /_____  / /____  ___  _____
-  \__ \/ __ \/ ___/ //_/ _ \/ __/ _ \/ _ \/ ___/
- ___/ / /_/ / /__/ ,< /  __/ /_/  __/  __/ /    
-/____/\____/\___/_/|_|\___/\__/\___/\___/_/     
-
-v1.0 - Made by Icarus | lolcat")
+os.system("cat src/banner.txt | lolcat")
+###
 
 def main():
-    print("Current network interface: ", os.popen("route | awk '/Iface/{getline; print $8}'").read())
+    try:
+
+        #Splash screen with iface info and current ip
+
+        def splash():
+            current_iface = os.popen("route | awk '/Iface/{getline; print $8}'").read()
+            current_addr  = os.popen("ip route get 1.2.3.4 | awk '{print $7}'").read()
+            current_ssid  = os.popen("iwgetid -r").read()
+            print("[Info → ] Current Interface:", current_iface) 
+            print("[Info → ] Current IP Address:", current_addr) 
+            print("[Info → ] Current SSID:", current_ssid)
+
+        
+        def shell():
+            splash()
+
+        shell()
 
 
-#Getting root privilages
+    except KeyboardInterrupt:
+        print("Exiting")
+
+# Getting root privilages
 
 rootchk = os.getuid()
 if rootchk != 0:
